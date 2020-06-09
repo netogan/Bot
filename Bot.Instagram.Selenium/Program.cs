ef754f1c9ad2187c72e457e949be1b19d1fa1f1b
netogan <netogan@gmail.com>
2020-06-08 23:33:49 -0300

    Atualização

---
+++ Bot.Instagram.Selenium/Program.cs
@@ -0,0 +1,55 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using Microsoft.Extensions.Configuration;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace Bot.Instagram.Selenium
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var regexRaiz = new Regex(@"(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+bin)");
+
+            var diretorioRaiz = regexRaiz.Match(AppDomain.CurrentDomain.BaseDirectory).Value;
+
+            var builder = new ConfigurationBuilder().SetBasePath(diretorioRaiz).AddJsonFile("appsettings.json");
+
+            var configuration = builder.Build();
+
+            IWebDriver driver = new ChromeDriver($"{diretorioRaiz}\\WebDriver");
+
+            try
+            {
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+
+                driver.Navigate().GoToUrl("https://www.instagram.com/accounts/login/");
+
+                var usuario = configuration["instagram:usuario"];
+                var senha = configuration["instagram:senha"];
+
+                wait.Until(ExpectedConditions.ElementExists(By.Name("username"))).SendKeys(usuario);
+                wait.Until(ExpectedConditions.ElementExists(By.Name("password"))).SendKeys(senha);
+                wait.Until(ExpectedConditions.ElementExists(By.CssSelector("button[type='submit']"))).Click();
+
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+
+                driver.Navigate().GoToUrl("https://www.instagram.com/netogan/");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                driver.Close();
+                driver.Dispose();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
